02688808b4bb73c84f53526d7b8e984cbc0af6bb
Ryan Jones <ryanjones33@gmail.com>
2019-05-14 16:26:57 -0700

    Enhance links and added typewriter effect

--- js/main.js
+++ js/main.js
@@ -15,10 +15,21 @@
 // }
 $(document).ready(() => {
   const name = 'Ryan Jones'
+  let typeName = '';
   $('#hero h1').click(function() {
     console.log(event.target.innerText)
   })
-
+  let count = 0;
+  (function typeOut () {
+    if (count < name.length) {
+      typeName += name[count];
+      console.log(typeName)
+      $('#type-out').text(typeName);
+      count ++
+      setTimeout(typeOut, 200)
+    }
+  })()
+  
   console.log('ready')
   $('#portfolio-flters').click(function() {
     event.preventDefault();
