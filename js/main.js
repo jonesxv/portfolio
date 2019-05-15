6c4580c1b18e8d9d0484fbf89f470db6c089a045
Ryan Jones <ryanjones33@gmail.com>
2019-05-15 13:16:01 -0700

    Fixed CSS bugs; Added animation

--- js/main.js
+++ js/main.js
@@ -15,6 +15,49 @@ $(document).ready(() => {
       setTimeout(typeOut, 200)
     }
   })()
+
+  // clip-path: polygon(0 0, 100% 0%, 100% 100%, 0 100%);
+  // clip-path: polygon(50 50, 50% 50%, 50% 50%, 50 50%);
+  // to
+  // clip-path: polygon(100% 70%, 26% 100%, 0 48%, 53% 0);
+  // clip-path: polygon(100% 42%, 26% 100%, 0 22%, 53% 0);
+  let a1 = 50;
+  let a2 = 50;
+  let b1 = 50;
+  let b2 = 50;
+  let c1 = 50;
+  let c2 = 50;
+  let d1 = 50;
+  let d2 = 40;
+  let interval = setInterval(function() {
+    a1++;
+    if (a2 > 42) {
+      a2--
+    }
+    if (b1 > 26) {
+      b1--;
+    }
+    if (b2 < 100) {
+      b2++;
+    }
+    if (c1 > 0) {
+      c1--;
+    }
+    if (c2 > 22) {
+      c2--;
+    }
+    if (d1 < 53) {
+      d1++;
+    }
+    if (d2 > 0) {
+      d2--;
+    }
+    $('#hero img').css('clip-path' , `polygon(${a1}% ${a2}%, ${b1}% ${b2}%, ${c1}% ${c2}%, ${d1}% ${d2}%)`)
+    if (a1 === 100) {
+      clearInterval(interval)
+    }
+  },50)
+  
   
   console.log('ready')
   $('#portfolio-flters').click(function() {
